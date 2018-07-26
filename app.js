ca83542285c8459da8705da23d4ce790b093cfca
James Feore <jafeore@microsoft.com>
2018-07-25 19:08:48 -0700

    Tested last message and changed twitter package

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const discordClient = new Discord.Client();
+const Twitter = require('twitter');
+const twitterClient = new Twitter({
+    consumer_key: process.env.CONSUMERKEY,
+    consumer_secret: process.env.CONSUMERSECRET,
+    access_token_key: process.env.TOKENKEY,
+    access_token_secret: process.env.TOKENSECRET
+});
 
 discordClient.on('ready', () => {
   console.log(`Logged in as ${discordClient.user.tag}!`);
@@ -9,8 +16,11 @@ discordClient.on('ready', () => {
 
 discordClient.on('message', msg => {
   if (msg.content === '!ping') {
-    msg.channel.sendreply('pong');
+    msg.channel.send('pong');
   }
+
+  console.log(`Last message: ${discordClient.user.lastMessage}`);
 });
 
 discordClient.login(process.env.DISCORDTOKEN);
+
